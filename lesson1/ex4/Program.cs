eb67810f73cebbf77524032d7963e61469369112
AlexandrZimakov <42317697+AlexandrZimakov@users.noreply.github.com>
2022-12-05 14:09:59 +0300

    HW1

--- lesson1/ex4/Program.cs
+++ lesson1/ex4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Write("Введите число:");
+int N = int.Parse(Console.ReadLine());
+int value = 1;
+
+while (value <= N) {
+    if (value % 2 == 0) {
+        Console.Write(value + " ");
+    }
+    value++;
+}
+Console.WriteLine();
