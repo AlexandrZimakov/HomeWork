beec7a8b4ac36e9e2f8880c33a1d10cddab58e52
AlexandrZimakov <42317697+AlexandrZimakov@users.noreply.github.com>
2022-12-06 17:17:34 +0300

    HW2

---
+++ lesson2/ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 13:Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать string[]
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+if (number < 100) {
+    Console.WriteLine("Третьей цифры нет.");
+    return;
+}
+
+while (number > 999) {
+    number = number / 10;
+}
+
+int result = number % 10;
+Console.WriteLine(result);
+
+
