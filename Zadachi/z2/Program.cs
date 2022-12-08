a95392a22a19824e83c29cb845ef34350f040953
AlexandrZimakov <42317697+AlexandrZimakov@users.noreply.github.com>
2022-12-08 15:38:59 +0300

    HW3

---
+++ Zadachi/z2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача No21. Работа в группах
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+● A (3,6); B (2,1) -> 5,09
+● A (7,-5); B (1,-1) -> 7,21*/
+
+Console.WriteLine("Введите координаты 1-й точки");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты 2-й точки");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+
+double firstX = GetDistance(x1, y1, x2, y2);
+
+double GetDistance(int x1, int y1, int x2, int y2) {
+    int calculation = (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
+    double result = Math.Sqrt((double)calculation);
+    return result;
+}
+Console.WriteLine(firstX);
---
+++ lesson3/ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 19Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.WriteLine("Введите пятизначное число:");
+int number =int.Parse(Console.ReadLine());
+
+if (isPolindrom(number)) {
+    Console.WriteLine("Ваше число - палиндром.");
+} else {
+    Console.WriteLine("Выше число - НЕ палиндром.");
+}
+bool isPolindrom(int number) {
+    int[] array = new int[5];
+    for (int i = 0; i < 5; i++) {
+        array [i] = number % 10;
+        number = number / 10;
+    }
+    if (   array[0] == array[4]
+        && array[1] == array [3] ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+}
