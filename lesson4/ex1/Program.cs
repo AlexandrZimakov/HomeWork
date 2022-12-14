12efc0464fb4ff62d98dc69f93a98259350600c8
a-zim <a-zimakov@mail.ru>
2022-12-14 14:41:04 +0300

    ex1

--- lesson4/ex1/Program.cs
+++ lesson4/ex1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetPower (int numberA,int numberB) {
+    int result = 1;
+    for (int i = 1; i <= numberB; i++) {
+        result = result*numberA;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(GetPower(numberA,numberB));
